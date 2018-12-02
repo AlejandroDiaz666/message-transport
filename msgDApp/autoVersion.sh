64e0bfb5eeace64c09a6eedd25262280bc1125ad
Alejandro Díaz <Alejandro.Daz@mailinator.com>
2018-12-02 10:14:08 -0800

    fix auto versiondepemdemcies... again

--- msgDApp/autoVersion.sh
+++ msgDApp/autoVersion.sh
@@ -5,5 +5,6 @@
 # version of the currently builing code from git. the module
 # will be src/autoVersion.js, which should be in .gitignore
 #
+BRANCH=$(git branch | grep "*" | cut -d " " -f 2)
 VER=$(git describe --tags --long)
-echo "var autoVersion = module.exports = { version() { return \"$VER\"; } }" > src/autoVersion.js
+echo "var autoVersion = module.exports = { version() { return \"$BRANCH $VER\"; } }" > src/autoVersion.js
